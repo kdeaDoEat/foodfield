b67c8d779ebfebce8506867d9837b8a6c09848b1
joungsik <winston91@naver.com>
2016-06-10 12:03:31 +0900

    chatting service

--- Food_Field/src/main/java/org/kdea/chatting/ChattingHandler.java
+++ Food_Field/src/main/java/org/kdea/chatting/ChattingHandler.java
@@ -15,106 +15,52 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class ChattingHandler extends TextWebSocketHandler {
 
-    public static Map<String, WebSocketSession> users = new ConcurrentHashMap();
-	
-    @Autowired
-    private ChattingService chatsvc;
-    
-    // 웹소켓 서버측에 텍스트 메시지가 접수되면 호출되는 메소드
-    @Override
-    public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        log("WebSocketSession ID : " + session.getId() + "메세지 문자 길이:" + message.getPayloadLength());
-        // textmessage
+	public static Map<String, WebSocketSession> users = new ConcurrentHashMap();
 
-        for (WebSocketSession s : users.values()) {
-           Map<String, Object> map = s.getAttributes();
-           String userId = (String) map.get("usrId");
-           System.out.println("message 보내는 중인 interceptor에서 건너온 ID " + userId);
-           if (message.getPayload().equals("/userrequest")) {
+	@Autowired
+	private ChattingService chatsvc;
 
-              List<String> userlist = new ArrayList<String>();
-              Iterator usernames = users.keySet().iterator();
-              String newsend = "/userrequest/";
-              while (usernames.hasNext()) {
+	// 웹소켓 서버측에 텍스트 메시지가 접수되면 호출되는 메소드
+	@Override
+	public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		log("WebSocketSession ID : " + session.getId() + "메세지 문자 길이:" + message.getPayloadLength());
+		// textmessage
+		chatsvc.broadcast(session, message, users);
+		log("보낸 메세지 " + message.getPayload());
 
-                 System.out.println("userlist while문");
-                 String user = (String) usernames.next();
-                 System.out.println(user);
-                 userlist.add(user);
-                 newsend += user + "/";
+	}
 
-              }
-              s.sendMessage(new TextMessage(newsend));
+	// 웹소켓 서버에 클라이언트가 접속하면 호출되는 메소드
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 
-           } else {
+		log("WebSocketSession ID : " + session.getId() + "현재 웹 소켓 세션 ID");
+		Map<String, Object> map = session.getAttributes();
+		String userId = (String) map.get("usrId");
+		System.out.println("afterconn interceptor에서 건너온 ID " + userId);
+		users.put(userId, session);
 
-              String[] msgarray = message.getPayload().split("/");
-              for (int i = 0; i < msgarray.length; i++) {
+	}
 
-                 System.out.println("메세지 분리 합니다~" + i + msgarray[i]);
+	// 클라이언트가 접속을 종료하면 호출되는 메소드
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		log("WebSocketSession ID : " + session.getId() + "현재 웹 소켓 세션 ID");
+		Map<String, Object> map = session.getAttributes();
+		String userId = (String) map.get("usrId");
+		System.out.println("afterclose interceptor에서 건너온 ID " + userId);
+		users.remove(userId);
+		System.out.println("클라이언트 접속해제");
+	}
 
-              }
-              
-              Map<String, Object> mymap = session.getAttributes();
-              String myId = (String) mymap.get("usrId");
-                            
-              //session.sendMessage(new TextMessage(myId+":"+msgarray[msgarray.length - 1]));
-              List<String> userlist = new ArrayList<String>();
-              Iterator usernames = users.keySet().iterator();
-              while (usernames.hasNext()) {
-                 String user = (String) usernames.next();
-                 for (int i = 0; i < msgarray.length - 1; i++) {
-                         if (user.equals(msgarray[i])) {
-                       WebSocketSession ss = users.get(user);
-                       if(s!=ss){
-                       ss.sendMessage(new TextMessage(myId+":"+msgarray[msgarray.length - 1]));
-                       }
-                    }
-                 }
-              }
+	// 메시지 전송시나 접속해제시 오류가 발생할 때 호출되는 메소드
+	@Override
+	public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+		super.handleTransportError(session, exception);
+		System.out.println("전송오류 발생");
+	}
 
-           }
-           log(userId + "가 보낸 메세지 " + message.getPayload());
-        }
-    }
- 
-    // 웹소켓 서버에 클라이언트가 접속하면 호출되는 메소드
-    @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-         
-        log("WebSocketSession ID : " + session.getId() + "현재 웹 소켓 세션 ID");
-
-        Map<String, Object> map = session.getAttributes();
-        String userId = (String) map.get("usrId");
-        System.out.println("afterconn interceptor에서 건너온 ID " + userId);
-        
-        users.put(userId, session);
-        
-    }
- 
-    // 클라이언트가 접속을 종료하면 호출되는 메소드
-    @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        log("WebSocketSession ID : " + session.getId() + "현재 웹 소켓 세션 ID");
-        Map<String, Object> map = session.getAttributes();
-        String userId = (String) map.get("usrId");
-        System.out.println("afterclose interceptor에서 건너온 ID " + userId);
-
-        //session에서 id가져오기 전
-        
-        users.remove(userId);
-        System.out.println("클라이언트 접속해제");
-    }
- 
-    // 메시지 전송시나 접속해제시 오류가 발생할 때 호출되는 메소드
-    @Override
-    public void handleTransportError(WebSocketSession session,
-            Throwable exception) throws Exception {
-        super.handleTransportError(session, exception);
-        System.out.println("전송오류 발생");
-    }
-    
-    private void log(String logmsg) {
-        System.out.println(new Date() + " : " + logmsg);
-     }
+	private void log(String logmsg) {
+		System.out.println(new Date() + " : " + logmsg);
+	}
 }
--- Food_Field/src/main/java/org/kdea/chatting/ChattingService.java
+++ Food_Field/src/main/java/org/kdea/chatting/ChattingService.java
@@ -1,5 +1,82 @@
 package org.kdea.chatting;
 
-public class ChattingService {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+public class ChattingService {
+    
+	/*맴버변수로 연결 후 autowired 하면 갱신 안됨*/
+	
+	public void broadcast(WebSocketSession session, TextMessage message, Map<String,WebSocketSession> users){
+		
+        for (WebSocketSession s : users.values()) {
+            Map<String, Object> map = s.getAttributes();
+            String userId = (String) map.get("usrId");
+            System.out.println("message 보내는 중인 interceptor에서 건너온 ID " + userId);
+            if (message.getPayload().equals("/userrequest")) {
+
+               List<String> userlist = new ArrayList<String>();
+               Iterator usernames = users.keySet().iterator();
+               String newsend = "/userrequest/";
+               while (usernames.hasNext()) {
+
+                  System.out.println("userlist while문");
+                  String user = (String) usernames.next();
+                  System.out.println(user);
+                  userlist.add(user);
+                  newsend += user + "/";
+
+               }
+               try {
+				s.sendMessage(new TextMessage(newsend));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+            } else {
+
+               String[] msgarray = message.getPayload().split("/");
+               for (int i = 0; i < msgarray.length; i++) {
+
+                  System.out.println("메세지 분리 합니다~" + i + msgarray[i]);
+
+               }
+               
+               Map<String, Object> mymap = session.getAttributes();
+               String myId = (String) mymap.get("usrId");
+                             
+               //session.sendMessage(new TextMessage(myId+":"+msgarray[msgarray.length - 1]));
+               List<String> userlist = new ArrayList<String>();
+               Iterator usernames = users.keySet().iterator();
+               while (usernames.hasNext()) {
+                  String user = (String) usernames.next();
+                  for (int i = 0; i < msgarray.length - 1; i++) {
+                          if (user.equals(msgarray[i])) {
+                        WebSocketSession ss = users.get(user);
+                        if(s!=ss){
+                        try {
+							ss.sendMessage(new TextMessage(myId+":"+msgarray[msgarray.length - 1]));
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+                        }
+                     }
+                  }
+               }
+
+            }
+        }
+		
+		
+	}
+	
 }
