4724e813a3312154db9697aa75f2a528ba47c332
joungsik <winston91@naver.com>
2016-06-10 11:38:56 +0900

    chatting&notice: session

--- Food_Field/src/main/java/org/kdea/chatting/ChattingInterceptor.java
+++ Food_Field/src/main/java/org/kdea/chatting/ChattingInterceptor.java
@@ -11,45 +11,43 @@ import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
-public class ChattingInterceptor extends HttpSessionHandshakeInterceptor{
-	
-    @Override
-    public boolean beforeHandshake(ServerHttpRequest request,
-            ServerHttpResponse response, WebSocketHandler wsHandler,
-            Map<String, Object> attributes) throws Exception {
-    	 
-        System.out.println("Before Handshake");
-                                                
-        ServletServerHttpRequest ssreq = (ServletServerHttpRequest) request;
-        System.out.println("URI:"+request.getURI());
- 
-        HttpServletRequest req =  ssreq.getServletRequest();
-                               
-        System.out.println("param, id:"+ ((UserVO)req.getSession().getAttribute("userInfo")).getNickname());
-        System.out.println(req.getParameter("_csrf"));
-         
-        if(req.getSession().getAttribute("userInfo")==null){
-        	
-        	System.out.println("채팅: 로그인 안하셨습니다.");
-        	return false;
-        }
-        
-        String usrId = ((UserVO)req.getSession().getAttribute("userInfo")).getNickname();
-        attributes.put("usrId", usrId);
-        
-        return super.beforeHandshake(request, response, wsHandler, attributes);
-     
-    }
-  
-    @Override
-    public void afterHandshake(ServerHttpRequest request,
-            ServerHttpResponse response, WebSocketHandler wsHandler,
-            Exception ex) {
-    	ServletServerHttpRequest ssreq = (ServletServerHttpRequest) request;
-    	HttpServletRequest req = ssreq.getServletRequest();
-        System.out.println("After Handshake, id: 접속자 : "+((UserVO)req.getSession().getAttribute("userInfo")).getNickname());
-        super.afterHandshake(request, response, wsHandler, ex);
-    }
-  
+public class ChattingInterceptor extends HttpSessionHandshakeInterceptor {
+
+	@Override
+	public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
+			Map<String, Object> attributes) throws Exception {
+
+		System.out.println("Before Handshake");
+
+		ServletServerHttpRequest ssreq = (ServletServerHttpRequest) request;
+		System.out.println("URI:" + request.getURI());
+
+		HttpServletRequest req = ssreq.getServletRequest();
+
+		if (req.getSession().getAttribute("userInfo") == null) {
+
+			System.out.println("채팅: 로그인 안하셨습니다.");
+			return false;
+		}
+
+		System.out.println("param, id:" + ((UserVO) req.getSession().getAttribute("userInfo")).getNickname());
+		System.out.println(req.getParameter("_csrf"));
+
+		String usrId = ((UserVO) req.getSession().getAttribute("userInfo")).getNickname();
+		attributes.put("usrId", usrId);
+
+		return super.beforeHandshake(request, response, wsHandler, attributes);
+
+	}
+
+	@Override
+	public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
+			Exception ex) {
+		ServletServerHttpRequest ssreq = (ServletServerHttpRequest) request;
+		HttpServletRequest req = ssreq.getServletRequest();
+		System.out.println(
+				"After Handshake, id: 접속자 : " + ((UserVO) req.getSession().getAttribute("userInfo")).getNickname());
+		super.afterHandshake(request, response, wsHandler, ex);
+	}
 
 }
